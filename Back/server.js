4f542129f61566a3adec05265ba25b80e83b9514
gonza laptop <gonz.r@hotmail.com>
2025-12-02 09:05:11 -0300

    feat: set up Express server with CORS and user routing, initialize database connection

--- Back/server.js
+++ Back/server.js
@@ -2,8 +2,11 @@ import express from "express";
 const app = express();
 app.disable("x-powered-by");
 const port = process.env.PORT || 3000;
-// router de usuarios
-// router de productos
+import { router as userRouter } from "./routes/users.js";
+//import { router as productRouter } from "./routes/products.js";
+import cors from "cors";
+import { connectDB } from "./db/sequelize.js";
+connectDB();
 
 app.use(cors({
     origin: [
@@ -19,7 +22,8 @@ app.use(cors({
 }));
 app.use(express.json());
 
-
+app.use('/users', userRouter);
+// router de productos
 
 
 
