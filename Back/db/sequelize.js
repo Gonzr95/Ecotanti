2bd6e38d040841b3e412eabd8842703e422be25d
gonza laptop <gonz.r@hotmail.com>
2025-12-02 09:01:47 -0300

    feat: implement user authentication with JWT, add user registration and login functionality, and integrate validation with Zod

---
+++ Back/db/sequelize.js
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+
+import dotenv from "dotenv";
+dotenv.config(); // Cargar variables desde .env
+
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ?? 3306,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log("Conexión a MySQL establecida correctamente.");
+  } catch (error) {
+    console.error("Error al conectar con MySQL:", error);
+    }
+};
+
+export { sequelize, connectDB };
